9116c03d450b0a13b6c7ab6745546593ca0e267a
Massive Twat <elikalliokoski@gmail.com>
2020-11-13 15:00:52 +0100

    Add project files.

---
+++ Rogue Company/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Rogue_Company
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.checkBox1.Location = new System.Drawing.Point(13, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(130, 29);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Trigger bot";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.maskedTextBox1);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 92);
+            this.panel1.TabIndex = 1;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(3, 54);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(166, 20);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(194, 98);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ Rogue Company/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+using AutoItX3Lib;
+using System.Runtime.InteropServices;
+// using ezOverLay;
+
+
+namespace Rogue_Company
+{
+    public partial class Form1 : Form
+    {
+      //  ez ez = new ez();
+
+        AutoItX3 au3 = new AutoItX3();
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey); 
+
+        object pix;
+        int newCol;
+        int[] ScreenCoor = { Screen.PrimaryScreen.Bounds.Width/2, Screen.PrimaryScreen.Bounds.Height/2 };
+        int x, y;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+         //   ez.SetInvi(this);
+         //   ez.StartLoop(10, "Rogue Company  ", this);
+            Thread tb = new Thread(Trigger) { IsBackground = true };
+            tb.Start();
+        }
+        private void Trigger()
+        {
+            while (true)
+            {
+                if (GetAsyncKeyState(Keys.F1) < 0)
+                {
+                    checkBox1.ForeColor = Color.Green;
+                    checkBox1.Checked = true;
+                }
+                if (GetAsyncKeyState(Keys.F2) < 0)
+                {
+                    checkBox1.ForeColor = Color.Red;
+                    checkBox1.Checked = false;
+                }
+                if (checkBox1.Checked)
+                {
+                    
+
+                    if (GetAsyncKeyState(Keys.XButton2)<0)
+                    {
+                           newCol = GrabColor();
+                        maskedTextBox1.Text = newCol.ToString();
+                        if (newCol > 16551953)
+                        {
+                            Thread.Sleep(1);
+                            au3.MouseClick("LEFT");
+                        }
+                    }
+                            
+                            if (GetAsyncKeyState(Keys.F10)<0)
+                    {
+                        this.Close();
+                    }
+                            
+                }
+
+                Thread.Sleep(1);
+            }
+        }
+
+        int GrabColor()
+        {
+            pix = au3.PixelGetColor(ScreenCoor[0], ScreenCoor[1]);
+             return Int32.Parse(pix.ToString());
+            
+        }
+    }
+}
